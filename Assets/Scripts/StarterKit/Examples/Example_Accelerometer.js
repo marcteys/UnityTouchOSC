eda1f6b41ec4405cb26f4b56c59f40d8682eb1c8
marcteys <marc.teys@gmail.com>
2013-10-11 23:33:42 +0100

    use accelerometer data - problem

--- Assets/Scripts/StarterKit/Examples/Example_Accelerometer.js
+++ Assets/Scripts/StarterKit/Examples/Example_Accelerometer.js
@@ -1,4 +1,3 @@
-#pragma strict
 private var oscData : OSCSimpleTarget;
 
 private var fader : float;
@@ -7,22 +6,33 @@ private var toggle : boolean;
 private var xyCoords : float[] = new float[2];
 private var AccelerometerXYZ : float[] = new float[3];
 
+var speed = 0.1;
+
+
 
 function Start () {
 	oscData = this.GetComponent(OSCSimpleTarget);
 	//	oscData = this.GetComponent(OSCMultipleTarget); // <- change if you want to access to the SimpleTarget or MultipleTarget script
 }
 
+
 function Update () {
 	//Get the data from OSCTarget script
 	GetData();
 	
 	// your code goes there...
+	var mapX = (AccelerometerXYZ[0]+10)*18 - 90;
+	var mapY = (AccelerometerXYZ[1]+10)*18 +180;
+	var mapZ = (AccelerometerXYZ[2]+10)*18;
 	
+	var desiredRotation = Quaternion.Euler(mapY, mapX, mapZ);
+
+	
+	transform.rotation =
+		  Quaternion.Slerp (transform.rotation, desiredRotation, Time.deltaTime * speed);
 	
 
-	var rotation = Quaternion.Euler(Vector3(AccelerometerXYZ[0], AccelerometerXYZ[1], AccelerometerXYZ[2]));
-	transform.rotation = rotation;	
+
 }
 
 
